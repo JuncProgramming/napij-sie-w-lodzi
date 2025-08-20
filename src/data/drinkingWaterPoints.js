7f94f5dcf8674ecaaa7fb0c7ec041e342abab343
Oliwier Juncewicz <122737448+JuncProgramming@users.noreply.github.com>
2025-08-20 23:15:02 +0200

    Added dark mode in Tailwind settings and updated the drinking water points data

---
+++ src/data/drinkingWaterPoints.js
@@ -0,0 +1,293 @@
+const drinkingWaterPoints = [
+  {
+    id: 'dw-1',
+    properties: {
+      name: 'Zdrój wody pitnej ZWiK Łódź "Przyklasztorze"'
+    },
+    geometry: {
+      type: 'Point',
+      coordinates: [19.470812130414583, 51.846288271311394],
+      placeId: 'ChIJ74r5SIzLG0cR5GEK7MGMQ0g'
+    }
+    /* Zdrój wody pitnej ZWiK Łódź "Przyklasztorze" */
+  },
+
+  {
+    id: 'dw-2',
+    properties: {
+      name: 'Zdrój wody pitnej ZWiK Łódź "Wycieczkowa"'
+    },
+    geometry: {
+      type: 'Point',
+      coordinates: [19.4878302, 51.822545],
+      placeId: 'ChIJqQNBUOLLG0cRv0utmHRJy2c'
+    }
+    /* Zdrój wody pitnej ZWiK Łódź "Wycieczkowa" */
+  },
+
+  {
+    id: 'dw-3',
+    properties: {
+      name: 'Zdrój wody pitnej ZWiK Łódź "Zbocze"'
+    },
+    geometry: {
+      type: 'Point',
+      coordinates: [19.5279717, 51.7783866],
+      placeId: 'ChIJ9eFxAyfLG0cRwv_YEsbHkko'
+    }
+    /* Zdrój wody pitnej ZWiK Łódź "Zbocze" */
+  },
+
+  {
+    id: 'dw-4',
+    properties: {
+      name: 'Zdrój wody pitnej przed siedzibą ZWiK Łódź przy ul. Wierzbowej 52'
+    },
+    geometry: {
+      type: 'Point',
+      coordinates: [19.479591, 51.769394]
+    }
+    /* Zdrój wody pitnej przed siedzibą ZWiK Łódź przy ul. Wierzbowej 52 */
+  },
+
+  {
+    id: 'dw-5',
+    properties: {
+      name: 'Zdrój wody pitnej w siedzibie ZWiK Łódź przy ul. Wierzbowej 52'
+    },
+    geometry: {
+      type: 'Point',
+      coordinates: [19.480922, 51.769518],
+      placeId: 'ChIJRWscUjDLG0cRGJHvhgyKHL0'
+    }
+    /* Zdrój wody pitnej w siedzibie ZWiK Łódź przy ul. Wierzbowej 52 */
+  },
+
+  {
+    id: 'dw-6',
+    properties: {
+      name: 'Zdrój wody pitnej przy skrzyżowaniu ul. Konstantynowskiej i ul. Unii Lubelskiej w Parku na Zdrowiu'
+    },
+    geometry: {
+      type: 'Point',
+      coordinates: [19.420932, 51.7667319]
+    }
+    /* Zdrój wody pitnej przy skrzyżowaniu ul. Konstantynowskiej i ul. Unii Lubelskiej w Parku na Zdrowiu */
+  },
+
+  {
+    id: 'dw-7',
+    properties: {
+      name: 'Zdrój wody pitnej na placu Wolności'
+    },
+    geometry: {
+      type: 'Point',
+      coordinates: [19.45528, 51.776512]
+    }
+    /* Zdrój wody pitnej na placu Wolności */
+  },
+
+  {
+    id: 'dw-8',
+    properties: {
+      name: 'Zdrój wody pitnej w Łódzkiej Informacji Turystycznej przy ul. Piotrkowskiej'
+    },
+    geometry: {
+      type: 'Point',
+      coordinates: [19.4556841, 51.7731619],
+      placeId: 'ChIJhQNZb9XKG0cR_1eIkFIfY4s'
+    }
+    /* Zdrój wody pitnej w Łódzkiej Informacji Turystycznej przy ul. Piotrkowskiej */
+  },
+
+  {
+    id: 'dw-9',
+    properties: {
+      name: 'Zdrój wody pitnej przy skrzyżowaniu ul. Piotrkowskiej i ul. Stefana Jaracza'
+    },
+    geometry: {
+      type: 'Point',
+      coordinates: [19.4557277, 51.7727836],
+      placeId: 'ChIJ31P5H93LG0cRk1OlSdm5AFg'
+    }
+    /* Zdrój wody pitnej przy skrzyżowaniu ul. Piotrkowskiej i ul. Stefana Jaracza */
+  },
+
+  {
+    id: 'dw-10',
+    properties: {
+      name: 'Zdrój wody pitnej przy ul. Piotrkowskiej (aleja Artura Rubinsteina)'
+    },
+    geometry: {
+      type: 'Point',
+      coordinates: [19.4564365, 51.7675838]
+    }
+    /* Zdrój wody pitnej przy ul. Piotrkowskiej (aleja Artura Rubinsteina) */
+  },
+
+  {
+    id: 'dw-11',
+    properties: {
+      name: 'Zdrój wody pitnej w Urzędzie Miasta Łodzi przy ul. Piotrkowskiej'
+    },
+    geometry: {
+      type: 'Point',
+      coordinates: [19.4576145, 51.764594],
+      placeId: 'ChIJY0E1N9Q0GkcRZIhC4V2nMlU'
+    }
+    /* Zdrój wody pitnej w Urzędzie Miasta Łodzi przy ul. Piotrkowskiej */
+  },
+
+  {
+    id: 'dw-12',
+    properties: {
+      name: 'Zdrój wody pitnej przy ul. Piotrkowskiej (aleja Leona Schillera)'
+    },
+    geometry: {
+      type: 'Point',
+      coordinates: [19.45786999, 51.7636942],
+      placeId: 'ChIJPcN5Ohs1GkcR-f8thdY_KQo'
+    }
+    /* Zdrój wody pitnej przy ul. Piotrkowskiej (aleja Leona Schillera) */
+  },
+
+  {
+    id: 'dw-13',
+    properties: { name: 'Zdrój wody pitnej przy ul. Piotrkowskiej' },
+    geometry: {
+      type: 'Point',
+      coordinates: [19.458131, 51.760966],
+      placeId: 'ChIJJwvSfos1GkcRH02YdYJrlxI'
+    }
+    /* Zdrój wody pitnej przy ul. Piotrkowskiej */
+  },
+
+  {
+    id: 'dw-14',
+    properties: {
+      name: 'Zdrój wody pitnej na terenie Manufaktury'
+    },
+    geometry: {
+      type: 'Point',
+      coordinates: [19.4411579, 51.7796711],
+      placeId: 'ChIJKd6At8XKG0cRtdyf4LNO9gw'
+    }
+    /* Zdrój wody pitnej na terenie Manufaktury */
+  },
+
+  {
+    id: 'dw-15',
+    properties: {
+      name: 'Zdrój wody pitnej przy ul. Ogrodowej (zachód)'
+    },
+    geometry: {
+      type: 'Point',
+      coordinates: [19.4491024, 51.7783386],
+      placeId: 'ChIJ21dUmI7LG0cRhuSOyiBAhKY'
+    }
+    /* Zdrój wody pitnej przy ul. Ogrodowej (zachód) */
+  },
+
+  {
+    id: 'dw-16',
+    properties: {
+      name: 'Zdrój wody pitnej przy ul. Ogrodowej (wschód)'
+    },
+    geometry: {
+      type: 'Point',
+      coordinates: [19.4451876, 51.777988]
+    }
+    /* Zdrój wody pitnej przy ul. Ogrodowej (wschód) */
+  },
+
+  {
+    id: 'dw-17',
+    properties: {
+      name: 'Zdrój wody pitnej przy ul. Legionów w pasażu Barbary Połomskiej'
+    },
+    geometry: {
+      type: 'Point',
+      coordinates: [19.4493743, 51.7766601],
+      placeId: 'ChIJDTzvVADLG0cROt_wJsT84ik'
+    }
+    /* Zdrój wody pitnej przy ul. Legionów w pasażu Barbary Połomskiej
+     */
+  },
+
+  {
+    id: 'dw-18',
+    properties: {
+      name: 'Zdrój wody pitnej na placu Henryka Dąbrowskiego'
+    },
+    geometry: {
+      type: 'Point',
+      coordinates: [19.4695314, 51.7726052]
+    }
+    /* Zdrój wody pitnej na placu Henryka Dąbrowskiego */
+  },
+
+  {
+    id: 'dw-19',
+    properties: {
+      name: 'Zdrój wody pitnej przy ul. Składowej (Nowa Fabryczna)'
+    },
+    geometry: {
+      type: 'Point',
+      coordinates: [19.471767, 51.770745],
+      placeId: 'ChIJFdvmHy_LG0cRV_mQkkFhd2w'
+    }
+    /* Zdrój wody pitnej przy ul. Składowej (Nowa Fabryczna) */
+  },
+
+  {
+    id: 'dw-20',
+    properties: {
+      name: 'Zdrój wody pitnej przy ul. płk. Jana Kilińskiego (Brama Miasta)'
+    },
+    geometry: {
+      type: 'Point',
+      coordinates: [19.465451, 51.7691]
+    }
+    /* Zdrój wody pitnej przy ul. płk. Jana Kilińskiego (Brama Miasta) */
+  },
+
+  {
+    id: 'dw-21',
+    properties: {
+      name: 'Zdrój wody pitnej w Zieleńcu przy ul. Junackiej'
+    },
+    geometry: {
+      type: 'Point',
+      coordinates: [19.521713, 51.795054],
+      placeId: 'ChIJ87eqjHbLG0cRz2KuLHSeWMQ'
+    }
+    /* Zdrój wody pitnej w Zieleńcu przy ul. Junackiej */
+  },
+
+  {
+    id: 'dw-22',
+    properties: {
+      name: 'Zdrój wody pitnej przy ul. 28 Pułku Strzelców Kaniowskich'
+    },
+    geometry: {
+      type: 'Point',
+      coordinates: [19.4407533, 51.7684853]
+    }
+    /* Zdrój wody pitnej przy ul. 28 Pułku Strzelców Kaniowskich */
+  },
+
+  {
+    id: 'dw-23',
+    properties: {
+      name: 'Zdrój wody pitnej w parku Podolskim'
+    },
+    geometry: {
+      type: 'Point',
+      coordinates: [19.49852, 51.74192],
+      placeId: 'ChIJFxZNDKQ0GkcRsc3QlyrIhRY'
+    }
+    /* Zdrój wody pitnej w parku Podolskim */
+  }
+];
+
+export default drinkingWaterPoints;
