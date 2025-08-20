92eef39ea0a6d4356281e0785007549c7f92a912
Oliwier Juncewicz <122737448+JuncProgramming@users.noreply.github.com>
2025-08-20 07:10:47 +0200

    Added a SidebarContext along with changes in main.jsx (now using the context). Modified App.jsx to make the map and header work together

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-import Map from "./components/Map";
+import Map from './components/Map';
+import Sidebar from './components/Sidebar';
+import Header from './components/Header';
 
 const App = () => {
-  return <div><Map></Map></div>;
+  return (
+    <>
+      <Sidebar />
+      <div className="flex h-screen w-screen flex-col overflow-hidden">
+        <Header />
+        <div className="flex-grow">
+          <Map />
+        </div>
+      </div>
+    </>
+  );
 };
-
 export default App;
