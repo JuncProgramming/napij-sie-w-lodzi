2db540e216d3a9c6181ce1f5ce333022f8cd992c
Oliwier Juncewicz <122737448+JuncProgramming@users.noreply.github.com>
2025-09-04 16:15:00 +0200

    Add an event handler to the WaterPointMarker component to handle the focus

--- src/components/WaterPointMarker.jsx
+++ src/components/WaterPointMarker.jsx
@@ -1,16 +1,34 @@
 import { Marker } from 'react-leaflet';
-import { memo } from 'react';
+import { memo, useRef } from 'react';
 import PopUp from './PopUp';
 
 const WaterPointMarker = memo(({ waterPoint, icon }) => {
+  const markerRef = useRef(null);
+
+  const handlePopupOpen = () => {
+    setTimeout(() => {
+      const popup = document.querySelector('.leaflet-popup-content');
+      if (popup) {
+        const firstButton = popup.querySelector('button');
+        if (firstButton) {
+          firstButton.focus();
+        }
+      }
+    }, 50);
+  };
+
   return (
     <Marker
+      ref={markerRef}
       position={[
         waterPoint.geometry.coordinates[1],
         waterPoint.geometry.coordinates[0]
       ]}
       icon={icon}
-      alt={`${waterPoint.properties.name}`}>
+      alt={`${waterPoint.properties.name}`}
+      eventHandlers={{
+        popupopen: handlePopupOpen
+      }}>
       <PopUp
         id={waterPoint.id}
         name={waterPoint.properties.name}
